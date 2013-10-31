c59c31692c51261476ca74d2fc15aed8d877ab80
witusj <wfth69@gmail.com>
2013-10-31 21:24:29 +0100

    A revised version of earlier attempt
    Now all records per city are parsed

---
+++ Crime1.R
@@ -0,0 +1,68 @@
+library("RCurl")
+library("XML")
+
+setwd("C:/Users/NB/Google Drive/Data Analysis/Inbraak Nijmegen")
+
+# Function to retrieve specific number from text
+getCrimeNumber <- function(text) {
+  
+  # Get table
+  CrimeData <- text
+  CrimeData
+  # Parse HTML into data frame and add variable names
+  r <- regexpr("<h2 class=\"listHeader\">(.*?)&nbsp", CrimeData)
+  
+  CrimeNumber <- regmatches(CrimeData, r)
+  CrimeNumber <- as.numeric(gsub("h2|[^0-9]", "", CrimeNumber, ignore.case = TRUE))
+}
+
+
+# Function to grab data from internet site according to given year
+getCrimeData <- function(town) {
+  
+  town <- as.character(town)
+  # Build url string
+  urlStr <- paste0("http://www.politie.nl/misdaad-in-kaart/lijst?geoquery=",town,",%20Nederland&categorie=1&categorie=2&pageSize=500")
+  
+  # Get table
+  CrimeData <- getURL(urlStr, ssl.verifypeer = FALSE)
+  
+  # Parse HTML into data frame
+  doc <- htmlParse(CrimeData)
+  tableNodes <- getNodeSet(doc, "//table")
+  tb <- readHTMLTable(tableNodes[[1]])
+  
+  # Retrieve number of burglaries
+  
+  CrimeCount <- getCrimeNumber(CrimeData)
+  i = 500
+  p = 2
+  
+  while (i < CrimeCount) {
+    
+    # Build url string
+    urlStr <- paste0("http://www.politie.nl/misdaad-in-kaart/lijst?geoquery=",town,",%20Nederland&categorie=1&categorie=2&pageSize=500&page=", p)
+    
+    # Get table
+    CrimeData2 <- getURL(urlStr, ssl.verifypeer = FALSE)
+    
+    doc2 <- htmlParse(CrimeData2)
+    tableNodes2 <- getNodeSet(doc2, "//table")
+    tb2 <- readHTMLTable(tableNodes2[[1]])
+    
+    tb <- rbind(tb, tb2)
+    
+    i = i + 500
+    p = p + 1
+  }
+
+  tb$Plaats <- rep(town,CrimeCount)
+  
+  tb  
+  
+}
+newdata <- c()
+for (t in c("Amsterdam", "Den Haag", "Rotterdam", "Utrecht", "Eindhoven", "Tilburg", "Groningen", "Breda", "Apeldoorn", "Nijmegen")){
+  data <- getCrimeData(t)
+  newdata <- rbind(newdata, data)
+}
